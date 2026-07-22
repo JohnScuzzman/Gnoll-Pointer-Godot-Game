5ef25f6a3637c07744a6f7c2d0d4fef95775bc2f
JohnScuzzman <scuzzyyysspamemail@gmail.com>
2026-07-22 11:55:08 -0400

    experimenting with room visibility

--- level/game_level.gd
+++ level/game_level.gd
@@ -8,3 +8,7 @@ var player_race
 
 func _ready() -> void:
 	debug_label.text = "Name: " + str(player_name) + " Class: " + str(player_class) + " Race: " + str(player_race)
+
+
+func _on_bar_trigger_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
